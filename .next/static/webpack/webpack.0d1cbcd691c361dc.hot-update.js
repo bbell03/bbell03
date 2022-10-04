5d11e05d81836a06b7c9062453ecab216bb83890
bbell03 <brandonbell.david@gmail.com>
2022-10-04 16:27:42 -0400

    update spacing

--- .next/static/webpack/webpack.0d1cbcd691c361dc.hot-update.js
+++ .next/static/webpack/webpack.0d1cbcd691c361dc.hot-update.js
@@ -11,8 +11,21 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1309bf845eac2509"; }
+/******/ 	__webpack_require__.h = function() { return "bf097129dbeb8a4b"; }
 /******/ }();
+/******/ 
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ 
+/******/ // noop fns to prevent runtime errors during initialization
+/******/ if (typeof self !== "undefined") {
+/******/ 	self.$RefreshReg$ = function () {};
+/******/ 	self.$RefreshSig$ = function () {
+/******/ 		return function (type) {
+/******/ 			return type;
+/******/ 		};
+/******/ 	};
+/******/ }
 /******/ 
 /******/ }
 );
--- .next/static/webpack/webpack.bf097129dbeb8a4b.hot-update.js
+++ .next/static/webpack/webpack.bf097129dbeb8a4b.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a108301a6e8ecba9"; }
+/******/ 	__webpack_require__.h = function() { return "ad06bc736e89b58c"; }
 /******/ }();
 /******/ 
 /******/ }
