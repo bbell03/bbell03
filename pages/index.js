258092e9c5abcd6a78d8e8f3096b39b4052ce1a6
bbell03 <brandonbell.david@gmail.com>
2022-10-04 16:50:21 -0400

    push even margin

--- pages/index.js
+++ pages/index.js
@@ -35,9 +35,9 @@ const Tails = () => {
   </div>
 </nav><section className="py-4 bg-white tails-selected-element">
     <div className="px-4 mx-auto text-center max-w-7xl sm:px-6 lg:px-8">
-        <h3 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl md:text-5xl xl:text-6xl">
+        <h4 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl md:text-5xl xl:text-6xl">
             Brandon D. Bell
-        </h3>
+        </h4>
         <HomeView/>
         <div className="flex justify-center mt-8 space-x-3">
         </div>
