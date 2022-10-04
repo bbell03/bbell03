ac5451e61a400e1a16fc38fafe04735b1c8f3841
bbell03 <brandonbell.david@gmail.com>
2022-10-04 16:48:22 -0400

    fix spacing again

--- components/ui_prototype/HomePagePhoto.js
+++ components/ui_prototype/HomePagePhoto.js
@@ -2,8 +2,8 @@
 
 const HomePagePhoto = () => {
   return (
-    <div class="sm:mt-2 xs:mt-2 grid grid-col-1 justify-center mx-6 object-fit">
-      <img class="lg:max-w-md md:max-w-md max-w-sm object-center rounded-full shadow-xl" src="/img/store.jpg" alt="Man looking at item at a store"></img>
+    <div class="sm:mt-2 xs:mt-2 grid grid-col-1 justify-center object-fit">
+      <img class="lg:max-w-md md:max-w-md max-w-sm object-center rounded-full shadow-xl mx-6" src="/img/store.jpg" alt="Man looking at item at a store"></img>
       <div className="flex justify-center mt-8 mb-8 space-x-3">
           <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow hover:bg-indigo-700" data-primary="indigo-600" data-rounded="rounded-md">Learn More</a>
           <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200" data-primary="indigo-600" data-rounded="rounded-md">Contact</a>
