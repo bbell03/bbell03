004b1a0dfa6e091b1b4f5d687d1a2b47728d5670
bbell03 <brandonbell.david@gmail.com>
2022-10-14 12:27:24 -0400

    push sizing

--- components/ui_prototype/HomePagePhoto.js
+++ components/ui_prototype/HomePagePhoto.js
@@ -4,7 +4,7 @@
 const HomePagePhoto = () => {
   return (
     <div class="sm:mt-2 xs:mt-2 grid grid-col-1 justify-center object-fit">
-      <img class="md:mt-12 lg:max-w-md md:max-w-sm max-w-sm object-center rounded-full shadow-xl" src="/img/store.jpg" alt="Man looking at item at a store"></img>
+      <img class="md:mt-12 lg:max-w-md md:max-w-md max-w-sm object-center rounded-full shadow-xl" src="/img/store.jpg" alt="Man looking at item at a store"></img>
       <div className="flex justify-center mt-8 mb-8 space-x-3">
 
       </div>
