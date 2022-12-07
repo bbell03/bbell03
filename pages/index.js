73dad5d58267ea8fcd0b6677c1822bbf40a59c5c
bbell03 <brandonbell.david@gmail.com>
2022-12-06 19:36:04 -0500

    new nav labels

--- pages/index.js
+++ pages/index.js
@@ -68,7 +68,7 @@ const Index = () => (
 
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Resume</a>
 
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Blog</a>
+            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Projects</a>
 
             <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Blog</a>
           </div>
