79dd24e1bb5fddae34209e03c131b59518762ba7
bbell03 <brandonbell.david@gmail.com>
2022-12-08 11:25:19 -0500

    sample footer and nav ui tweaks

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b2f2de58757aba02"; }
+/******/ 		__webpack_require__.h = function() { return "fbc5c6aa796bfab2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
