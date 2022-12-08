d448480acbdabf8189e34b1bdceaf462a672a9a3
bbell03 <brandonbell.david@gmail.com>
2022-12-08 11:19:47 -0500

    sample footer and nav ui tweaks

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "651ab4455e113132"; }
+/******/ 		__webpack_require__.h = function() { return "e88ed713cf8e8de4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ components/ui_prototype/Footer.js
@@ -0,0 +1,29 @@
+const Footer = () => {
+  return (
+
+
+<footer class="fixed bottom-0 left-0 z-20 p-4 w-full bg-white border-t border-white-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-white-800 dark:border-gray-600">
+    <span class="text-sm text-black-500 sm:text-center dark:text-black-400">© 2022 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-black-500 dark:text-black-400 sm:mt-0">
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">Home</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">About</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Resume</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Projects</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Blog</a>
+        </li>
+    </ul>
+</footer>
+
+)};
+
+export default Footer;
