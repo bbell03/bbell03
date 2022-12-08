4405252abcc71d603579cf70d685b2e77b91e5cd
bbell03 <brandonbell.david@gmail.com>
2022-12-08 12:25:09 -0500

    shorter blog posts

--- components/ui_prototype/BlogPost.js
+++ components/ui_prototype/BlogPost.js
@@ -5,7 +5,7 @@ const BlogPost = () => {
         <div class="p-8">
           <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
           <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-          <p class="mt-2 text-slate-500">Getting a new business off the ground is a lot of work. </p>
+          <p class="mt-2 text-slate-500">Getting a business off the ground takes work. </p>
         </div>
       </div>
     </div>
