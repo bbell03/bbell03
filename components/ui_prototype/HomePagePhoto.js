6582c4bc2b4da94e929485f3d479f0a1c73ffcf2
bbell03 <bbell03@tufts.edu>
2022-12-14 18:05:41 -0500

    mobile photo change

--- components/ui_prototype/HomePagePhoto.js
+++ components/ui_prototype/HomePagePhoto.js
@@ -3,7 +3,7 @@
 // <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200" data-primary="indigo-600" data-rounded="rounded-md">Contact</a>
 const HomePagePhoto = () => {
   return (
-    <div class="sm:mt-2 xs:mt-2 grid grid-col-1 justify-center object-fit">
+    <div class="xs:mx-2 sm:mt-2 xs:mt-2 grid grid-col-1 justify-center object-fit">
       <img class="md:mt-12 lg:max-w-md md:max-w-sm max-w-sm object-center rounded-full shadow-xl" src="/img/store.jpg" alt="Man looking at item at a store"></img>
       <div className="flex object-center justify-center mt-8 mb-8 space-x-3">
           <label for="toggle-example" class="flex items-center cursor-pointer relative mb-4">
