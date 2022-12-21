ab8583e034d8051d769d695651dba12bffd9df9d
bbell03 <bbell03@tufts.edu>
2022-12-20 22:15:18 -0500

    upper case filename

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import HomeView from '.././components/HomeView';
 import AppBar from '.././components/material/appbar';
 import Footer from '.././components/ui_prototype/Footer';
-import Nav from '.././components/nav';
+import Nav from '.././components/Nav';
 // <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow hover:bg-indigo-700" data-primary="indigo-600" data-rounded="rounded-md">Learn More</a>
 // <a href="#" className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-indigo-700 bg-indigo-100 border border-transparent rounded-md hover:bg-indigo-200" data-primary="indigo-600" data-rounded="rounded-md">Contact</a>
 
