6cf466f9f798d3114ff4244be4914c7c7e3225b0
bbell03 <bbell03@tufts.edu>
2023-01-13 17:58:42 -0500

    remove shadow

--- components/Nav.js
+++ components/Nav.js
@@ -34,7 +34,7 @@ const Nav = () => (
           </div>
         </div>
       <div class="rounded-lg absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-        <div class="flex mt-4 space-x-2 sm:justify-center sm:mt-0 px-2 py-2 rounded-md shadow-xl sm:shadow-none">
+        <div class="flex mt-4 space-x-2 sm:justify-center sm:mt-0 px-2 py-2 rounded-md">
                     <a href="#" class="text-gray-700 hover:text-gray-900 dark:hover:text-indigo-500">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-5 h-5"><path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/></svg>
                     </a>
