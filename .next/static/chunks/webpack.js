8a92cd4a37b8cfb55424890c0d448931e3789b87
bbell03 <brandonbell.david@gmail.com>
2023-01-21 16:36:34 -0500

    push spacing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "55b5c4ac3664db5c"; }
+/******/ 		__webpack_require__.h = function() { return "243933e1ee53f0cb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/HomeView.js
+++ components/HomeView.js
@@ -6,14 +6,14 @@ import IntroModule from ".././pages/Intro.jsx";
 //Flex might be better you might be able to avoid all of the manual spanning
 const HomeView = () => {
   return(
-    <div class = "mb-4 mt-0 md:mt-0 lg:mt-0 xl:mt-0 flex items-center object-scale-down">
+    <div class = "mb-2 mt-0 md:mt-0 lg:mt-0 xl:mt-0 flex items-center object-scale-down">
       <div class="flex-1 max-w-full max-w-4xl mx-auto items-center">
             <ul class="mt-4 grid grid-cols-1 md:grid-cols-5 lg:grid-cols-5 lg:gap-x-72 md:gap-x-64 gap-x-56 justify-center place-items-center">
               <li class="sm:col-start-2 md:col-start-2 lg:col-start-2 col-span-1"><HomePagePhoto/></li>
               <li class="col-span-1 sm:col-span-3 md:col-span-3 lg:col-span-3"><NewsFeed/></li>
             </ul>
-                        <h1 className="mt-4 hidden sm:block sm:mt-2 mb-4 text-5xl font-bold text-left">Hi I'm Brandon.</h1>
-                        <hr className="mt-6 hidden sm:block"/>
+                        <h1 className="hidden sm:block sm:mt-2 mb-4 text-5xl font-bold text-left">Hi I'm Brandon.</h1>
+                        <hr className="mt-4 hidden sm:block"/>
       </div>
     </div>
   );
