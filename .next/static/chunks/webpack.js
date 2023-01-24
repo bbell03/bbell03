a785bb685f191e8fc2b606905f985009139219c3
bbell03 <bbell03@tufts.edu>
2023-01-24 17:44:24 -0500

    margin

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9ef3e580f0873445"; }
+/******/ 		__webpack_require__.h = function() { return "521f88cffdb2cea3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.6140bce549e5ab1d.hot-update.js
+++ .next/static/webpack/webpack.6140bce549e5ab1d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "56c284acdefec2e0"; }
+/******/ 	__webpack_require__.h = function() { return "23e0c31e95194270"; }
 /******/ }();
 /******/ 
 /******/ }
