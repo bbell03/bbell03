6107193ac44ec49e5c91aade5afdbffe42e672bf
bbell03 <bbell03@tufts.edu>
2023-01-24 14:29:59 -0500

    desktop 1440p sizing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a7c7b152af48bd73"; }
+/******/ 		__webpack_require__.h = function() { return "7898afc16790b55f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ import Nav from '.././components/Nav';
   //pass children to navbar for different site contexts
 
 const Index = () => (
-
   <div className = "font-sofia min-w-xs pt-2 bg-white px-8 mx-auto max-w-7xl lg:px-16">
       <Nav/>
     <section className="pt-4 bg-white tails-selected-element">
@@ -34,17 +33,35 @@ const Index = () => (
             <h2 className="mb-4 text-2xl font-bold md:text-3xl"></h2>
             <div className="grid grid-cols-1 gap-0 text-gray-600 md:grid-cols-2 md:gap-16">
               <div>
-                <h4 className="mt-10 mb-3 font-semibold text-gray-900">Who am I?</h4>
-                <p  className = "text-lg">I am a software developer currently based in NYC. I graduated from Tufts University with a Bachelor of Arts in Computer Science. I also have interests in Spanish, real estate, investment, food, chemistry, fitness, and design.</p>
-                <h2 className="mt-10 mb-3 font-semibold text-gray-900">What have I done?</h2>
-                <p className = "text-lg">Work ranging from full-stack web development in Javascript, HTML and CSS, to data structures and algorithms in C++, to artificial intelligence in Python, to low level implementations in C. Most notably, I have implemented a ride sharing service using Google Maps, a presidential campaign tracker,
-                                        an image compressor, a MERN stack, and a simple virtual machine. </p>
+                <h4 className="mt-12 mb-3 font-semibold text-gray-900">Who am I?</h4>
+                <p  className = "text-lg sm:text-xl">
+                  I am a software developer currently based in NYC.
+                  I graduated from Tufts University with a Bachelor of Arts in Computer Science.
+                  I also have interests in Spanish, real estate, investment, food, chemistry,
+                  fitness, and design.
+                </p>
+                <h2 className="mt-12 mb-3 font-semibold text-gray-900">What have I done?</h2>
+                <p className = "text-lg sm:text-xl">
+                  Work ranging from full-stack web development in Javascript,
+                  HTML and CSS, to data structures and algorithms in C++, to artificial intelligence in Python,
+                  to low level implementations in C. Most notably, I have implemented a ride sharing service
+                  using Google Maps, a presidential campaign tracker, an image compressor, a MERN stack,
+                  and a simple virtual machine.
+                </p>
               </div>
             <div className="">
-                <h4 className="mt-10 mb-3 font-semibold text-gray-900">What am I looking for?</h4>
-                <p  className = "text-lg">I am looking for  opportunities in software engineering that value a well principled and iterative workflow, healthy challenge, good code, and strong design ideals. I am looking for ways to grow and improve through the work that I do so as to bridge my own interests.</p>
-                <h4 className="mt-10 mb-3 font-semibold text-gray-900">What is the meaning of this?</h4>
-                <p  className = "text-lg">This site is a place to share my interests, and how I look to develop them. I would ask that you take care while looking through as there may yet be inconsistencies, points of improvement or possible directions to consider.</p>
+                <h4 className="mt-12 mb-3 font-semibold text-gray-900">What am I looking for?</h4>
+                <p  className = "text-lg sm:text-xl">
+                  I am looking for  opportunities in software engineering that value a
+                  well principled and iterative workflow, healthy challenge, good code, and strong design ideals. \
+                  I am looking for ways to grow and improve through the work that I do so as to bridge my own interests.
+                </p>
+                <h4 className="mt-12 mb-3 font-semibold text-gray-900">What is the meaning of this?</h4>
+                <p  className = "text-lg sm:text-xl">
+                  This site is a place to share my interests, and how I look to
+                  develop them. I would ask that you take care while looking through as there may yet be inconsistencies,
+                   points of improvement or possible directions to consider.
+                </p>
             </div>
         </div>
 
