8a871a3091a84ca65d7c8c7f1186a563656e64ef
Brandon Bell <brandonbell@macbook-air.mynetworksettings.com>
2023-11-22 00:23:24 -0500

    blog less comment space

--- pages/blog.js
+++ pages/blog.js
@@ -40,7 +40,7 @@ const Blog = () => {
               <form class="mb-6">
                   <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-200 dark:border-gray-200">
                       <label for="comment" class="sr-only">Your comment</label>
-                      <textarea id="comment" rows="6"
+                      <textarea id="comment" rows="1"
                           class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 dark:text-black dark:placeholder-200-400 dark:bg-gray-200"
                           placeholder="Write a comment..." required></textarea>
                   </div>
